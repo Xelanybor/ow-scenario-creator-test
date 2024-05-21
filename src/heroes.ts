7b73395479370a822187e440447443c02e5faa13
Alex Smith <xelanybor@gmail.com>
2024-05-21 15:14:28 +0200

    added support for setting heroes

---
+++ src/heroes.ts
@@ -0,0 +1,64 @@
+enum Hero {
+    Ana = 'Ana',
+    Ashe = 'Ashe',
+    Baptiste = 'Baptiste',
+    Bastion = 'Bastion',
+    Brigitte = 'Brigitte',
+    Cassidy = 'Cassidy',
+    Dva = 'D.Va',
+    Doomfist = 'Doomfist',
+    Echo = 'Echo',
+    Genji = 'Genji',
+    Hanzo = 'Hanzo',
+    Illari = 'Illari',
+    JunkerQueen = 'Junker Queen',
+    Junkrat = 'Junkrat',
+    Kiriko = 'Kiriko',
+    Lifeweaver = 'Lifeweaver',
+    Lucio = 'Lúcio',
+    Mauga = 'Mauga',
+    Mei = 'Mei',
+    Mercy = 'Mercy',
+    Moira = 'Moira',
+    Orisa = 'Orisa',
+    Pharah = 'Pharah',
+    Ramattra = 'Ramattra',
+    Reaper = 'Reaper',
+    Reinhardt = 'Reinhardt',
+    Roadhog = 'Roadhog',
+    Sigma = 'Sigma',
+    Sojourn = 'Sojourn',
+    Soldier76 = 'Soldier: 76',
+    Sombra = 'Sombra',
+    Symmetra = 'Symmetra',
+    Torbjorn = 'Torbjörn',
+    Tracer = 'Tracer',
+    Widowmaker = 'Widowmaker',
+    Winston = 'Winston',
+    WreckingBall = 'Wrecking Ball',
+    Zarya = 'Zarya',
+    Zenyatta = 'Zenyatta'
+}
+
+function setHeroes(team1: Hero[], team2: Hero[]): string {
+    return `    heroes
+    {
+        Team 1
+        {
+            enabled heroes
+            {
+                ${team1.map(hero => hero.valueOf()).join('\n                ')}
+            }
+        }
+
+        Team 2
+        {
+            enabled heroes
+            {
+                ${team2.map(hero => hero.valueOf()).join('\n                ')}
+            }
+        }
+    }`
+}
+
+export { Hero, setHeroes };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Rule from "./rule.js";
+import { Hero, setHeroes } from "./heroes.js";
 
 let r = new Rule(
     "Rule 2",
@@ -6,4 +7,9 @@ let r = new Rule(
     "Is Button Held(Event Player, Button(Interact)) == True;Is Button Held(Event Player, Button(Melee)) == True;",
     "Call Subroutine(init0);"
 );
-console.log(r.toString());
+// console.log(r.toString());
+
+let team1 = [Hero.JunkerQueen, Hero.Cassidy, Hero.Bastion, Hero.Moira, Hero.Baptiste];
+let team2 = [Hero.Doomfist, Hero.Tracer, Hero.Sombra, Hero.Ana, Hero.Brigitte];
+
+console.log(setHeroes(team1, team2));
