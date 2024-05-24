f3e3ede51583f5ba13403e6550b4565a680c9610
Alex Smith <xelanybor@gmail.com>
2024-05-24 16:13:59 +0200

    implemented a dropdown to select a hero

--- src/scripts/create_scenario.ts
+++ src/scripts/create_scenario.ts
@@ -6,7 +6,7 @@ let scenario = new Scenario();
 
 scenario.setMap(OverwatchMap.Oasis);
 scenario.setPoint(2);
-scenario.setTeam1([Hero.Cassidy], [[-192.270, 17.426, 12.783]], [[1, 0, -1]], [true]);
+scenario.setTeam1([Hero.Cassidy, Hero.Bastion], [[-192.270, 17.426, 12.783], [-192.270, 17.426, 12.783]], [[1, 0, -1], [1, 0, -1]], [true, true]);
 scenario.setTeam2([Hero.Venture], [[-192.270, 17.426, 12.783]], [[-1, 0, 1]], [false]);
 
 let b = document.getElementById("testButton");
@@ -17,8 +17,19 @@ if (!b) {
 }
 b.onclick = printCode;
 
-export function printCode() {
-    console.log(scenario.generateCode());
+let d = document.createElement("select");
+d.id = "heroSelect";
+document.body.appendChild(d);
+
+for (let hero of Object.keys(Hero)) {
+    let o = document.createElement("option");
+    o.value = hero;
+    o.text = (Hero as any)[hero];
+    d.appendChild(o);
 }
 
-console.log(document);
+export function printCode() {
+    let hero = (Hero as any)[(document.getElementById("heroSelect") as HTMLSelectElement).value];
+    scenario.setTeam1([hero], [[-192.270, 17.426, 12.783]], [[1, 0, -1]], [true]);
+    console.log(scenario.generateCode());
+}
